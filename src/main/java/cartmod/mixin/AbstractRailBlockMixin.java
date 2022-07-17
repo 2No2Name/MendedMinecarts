9b09b0a67558de58a10e8c1263da1fbc37a11b75
2No2Name <2No2Name@web.de>
2022-07-17 15:20:31 +0200

    Replace three rail block mixins with one

--- src/main/java/cartmod/mixin/AbstractRailBlockMixin.java
+++ src/main/java/cartmod/mixin/AbstractRailBlockMixin.java
@@ -2,35 +2,36 @@ package cartmod.mixin;
 
 import cartmod.RailHitboxHelper;
 import net.minecraft.block.AbstractRailBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.RailBlock;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.block.enums.RailShape;
-import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(RailBlock.class)
-public abstract class RailBlockMixin extends AbstractRailBlock{
-    @Shadow @Final public static EnumProperty<RailShape> SHAPE;
+@Mixin(AbstractRailBlock.class)
+public abstract class AbstractRailBlockMixin extends Block {
 
-    protected RailBlockMixin(boolean forbidCurves, Settings settings) {
-        super(forbidCurves, settings);
+    @Shadow
+    public abstract Property<RailShape> getShapeProperty();
+
+    public AbstractRailBlockMixin(Settings settings) {
+        super(settings);
     }
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         VoxelShape railCollisionShape = super.getOutlineShape(state, world, pos, context);
-        return RailHitboxHelper.getOutlineShape(railCollisionShape, state, state.get(SHAPE), world, pos, context);
+        return RailHitboxHelper.getOutlineShape(railCollisionShape, state, state.get(this.getShapeProperty()), world, pos, context);
     }
 
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         VoxelShape railCollisionShape = super.getCollisionShape(state, world, pos, context);
-        return RailHitboxHelper.getCollisionShape(railCollisionShape, state.get(SHAPE), state, world, pos, context);
+        return RailHitboxHelper.getCollisionShape(railCollisionShape, state.get(this.getShapeProperty()), state, world, pos, context);
     }
 }
