475c6f8386019b60ee6ca6a0b54a7ef967491947
2No2Name <2No2Name@web.de>
2022-07-19 01:19:48 +0200

    fix: handle packets like other packets

--- src/main/java/cartmod/mixin/settings/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/cartmod/mixin/settings/ClientPlayNetworkHandlerMixin.java
@@ -2,22 +2,28 @@ package cartmod.mixin.settings;
 
 import cartmod.settings.SettingSync;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 
     @Inject(
             method = "onCustomPayload",
-            at = @At("HEAD")
+            at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V", shift = At.Shift.BEFORE),
+            cancellable = true,
+            locals = LocalCapture.CAPTURE_FAILHARD
     )
-    public void onCustomPayload(CustomPayloadS2CPacket packet, CallbackInfo ci) {
+    public void onCustomPayload(CustomPayloadS2CPacket packet, CallbackInfo ci, Identifier identifier, PacketByteBuf packetByteBuf) {
         if (packet.getChannel().equals(SettingSync.CHANNEL)) {
-            SettingSync.handleData(packet.getData());
+            ci.cancel();
+            SettingSync.handleData(packetByteBuf);
         }
     }
 }
