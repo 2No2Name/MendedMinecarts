b6953ad73641c6e20e5ebe5afba57c589fbb671d
2No2Name <2No2Name@web.de>
2022-07-19 22:24:18 +0200

    fix: avoid 30bps speed limit of minecarts

--- src/main/java/mendedminecarts/mixin/AbstractMinecartEntityMixin_ConfiguredSpeed.java
+++ src/main/java/mendedminecarts/mixin/AbstractMinecartEntityMixin_ConfiguredSpeed.java
@@ -5,6 +5,8 @@ import net.minecraft.entity.vehicle.AbstractMinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractMinecartEntity.class)
@@ -17,12 +19,20 @@ public abstract class AbstractMinecartEntityMixin_ConfiguredSpeed {
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;getMaxSpeed()D"
-    )
+            )
     )
     public double getMaxSpeed(AbstractMinecartEntity instance) {
         if (MendedMinecartsMod.CUSTOM_RAIL_SPEED.isDefault()) {
             return this.getMaxSpeed();
         }
         return this.getMaxSpeed() * (MendedMinecartsMod.CUSTOM_RAIL_SPEED.getState() / (8d / 20d));
+    }
+
+    @ModifyConstant(method = "moveOnRail", constant = @Constant(doubleValue = 2.0, ordinal = 0))
+    private double scaleSpeedLimit(double original) {
+        if (MendedMinecartsMod.CUSTOM_RAIL_SPEED.isDefault()) {
+            return original;
+        }
+        return original * (MendedMinecartsMod.CUSTOM_RAIL_SPEED.getState() / (8d / 20d));
     }
 }
