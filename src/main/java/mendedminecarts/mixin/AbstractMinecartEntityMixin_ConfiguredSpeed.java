b57eede6a04dda48a7648eed224690b6e49ec5d2
2No2Name <2No2Name@web.de>
2022-07-20 16:20:46 +0200

    fix: super acceleration of fast minecarts

--- src/main/java/mendedminecarts/mixin/AbstractMinecartEntityMixin_ConfiguredSpeed.java
+++ src/main/java/mendedminecarts/mixin/AbstractMinecartEntityMixin_ConfiguredSpeed.java
@@ -2,12 +2,11 @@ package mendedminecarts.mixin;
 
 import mendedminecarts.MendedMinecartsMod;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartEntityMixin_ConfiguredSpeed {
@@ -34,5 +33,44 @@ public abstract class AbstractMinecartEntityMixin_ConfiguredSpeed {
             return original;
         }
         return original * (MendedMinecartsMod.CUSTOM_RAIL_SPEED.getState() / (8d / 20d));
+    }
+
+    //Fix for super acceleration:
+    @ModifyVariable(
+            method = "moveOnRail",
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;floor(D)I", ordinal = 0),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;setVelocity(DDD)V", ordinal = 0)
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;",
+                    ordinal = 1,
+                    shift = At.Shift.BEFORE
+            ),
+            ordinal = 0
+    )
+    private int clampXDistance(int x, BlockPos pos) {
+        int difference = MathHelper.clamp(x - pos.getX(), -1, 1);
+        return pos.getX() + difference;
+    }
+
+    @ModifyVariable(
+            method = "moveOnRail",
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;floor(D)I", ordinal = 0),
+                    to = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;setVelocity(DDD)V", ordinal = 0)
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;",
+                    ordinal = 1,
+                    shift = At.Shift.BEFORE
+            ),
+            ordinal = 1
+    )
+    private int clampZDistance(int z, BlockPos pos) {
+        int difference = MathHelper.clamp(z - pos.getZ(), -1, 1);
+        return pos.getZ() + difference;
     }
 }
