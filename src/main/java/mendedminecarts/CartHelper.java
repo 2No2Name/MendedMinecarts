e9975ac02ab1fb22fa9ac2c25654c23440e9be9c
2No2Name <2No2Name@web.de>
2022-07-20 16:42:08 +0200

    fix: max cart speed setting used in display and estimated distance

--- src/main/java/mendedminecarts/CartHelper.java
+++ src/main/java/mendedminecarts/CartHelper.java
@@ -7,9 +7,15 @@ public class CartHelper {
 
 
     public static double getMaxSpeed(AbstractMinecartEntity entity) {
+        double maxSpeed;
         if (entity instanceof FurnaceMinecartEntity) {
-            return (entity.isTouchingWater() ? 3.0 : 4.0) / 20.0;
+            maxSpeed = (entity.isTouchingWater() ? 3.0 : 4.0) / 20.0;
+        } else {
+            maxSpeed = (entity.isTouchingWater() ? 4.0 : 8.0) / 20.0;
         }
-        return (entity.isTouchingWater() ? 4.0 : 8.0) / 20.0;
+        if (MendedMinecartsMod.CART_SPEED.isDefault()) {
+            return maxSpeed;
+        }
+        return maxSpeed * (MendedMinecartsMod.CART_SPEED.getState() / (8d / 20d));
     }
 }
