67dad8e1dba15f143513765a2aae462e34bdb4f7
2No2Name <2No2Name@web.de>
2023-07-20 21:39:37 +0200

    change: RailPlacement without updates

---
+++ src/main/java/mendedminecarts/RailPlacementHelper.java
@@ -0,0 +1,8 @@
+package mendedminecarts;
+
+import net.minecraft.util.math.BlockPos;
+
+public class RailPlacementHelper {
+    public static final ThreadLocal<BlockPos> NO_CONNECT_POS = new ThreadLocal<>();
+
+}
